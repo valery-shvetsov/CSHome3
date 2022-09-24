05d87d490f6c27bd8f11f50304eba93e2274c149
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-24 22:52:46 +0300

    ex19

---
+++ ex19/Program.cs
@@ -0,0 +1,38 @@
+﻿//проверка числа полиндром или нет
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    int number = int.Parse(Console.ReadLine() ?? "");
+    return number;
+}
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+string PolyTest(int number)
+{
+    if (number / 10000 == number % 10 && (number / 1000) % 10 == (number / 10) % 10)
+    {
+        return ("Число полиндром");
+    }
+    else
+    {
+        return ("Число неполиндром");
+    }
+}
+// основная программа
+int number = ReadData("Введите пятизначное число: ");
+if (number >= 10000 && number < 100000)
+{
+    string answer = PolyTest(number);
+    PrintData(answer, "");
+}
+else
+{
+    PrintData("Число не пятизначное", "");
+}
+
+
+
+
