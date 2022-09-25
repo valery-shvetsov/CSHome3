6fe6976bcfc41c6de4af4e224c534046ed37c24e
valery-shvetsov <113056546+valery-shvetsov@users.noreply.github.com>
2022-09-25 13:54:27 +0300

    ex23

---
+++ ex23/Program.cs
@@ -0,0 +1,27 @@
+﻿// Вывод таблицы кубов
+int ReadData(string Line)
+{
+    Console.WriteLine(Line);
+    int number = int.Parse(Console.ReadLine()??"");
+    return number;
+}
+void PrintData(string prefix, string value)
+{
+    Console.WriteLine(prefix + value);
+}
+string LineNum(int numN, int p)
+{
+    string outLine = "1";
+    for (int i = 2; i<=numN; i++)
+    {
+        outLine = outLine + " " + Math.Pow(i,p);
+    }
+    return outLine;
+}
+//main
+
+int numN=ReadData("Введите число N: ");
+string LineTop=LineNum(numN,1);
+string LineDoun=LineNum(numN,3);
+PrintData("",LineTop);
+PrintData("",LineDoun);
